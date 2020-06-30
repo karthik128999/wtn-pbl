bd1dcd4136e4e63d70b43a85e390b0ed36918ecd
karthik <karthik@karthik>
2020-06-30 18:04:58 +0530

    first

---
+++ WTN_MILESTONE1/src/exceptions/Userregistration.java
@@ -0,0 +1,36 @@
+package exceptions;
+class InvalidCountryException extends Exception
+{
+	InvalidCountryException()
+	{
+		System.out.println("invalid");
+	}
+	InvalidCountryException(String s)
+	{
+		System.out.println(s);
+	}
+}
+public class Userregistration {
+void registerUser(String uname,String ucountry) throws InvalidCountryException
+{
+	if(!ucountry.equals("India"))
+	{
+		throw new InvalidCountryException("user outside india");
+	}
+	else
+	{
+		System.out.println("successfull");
+	}
+}
+public static void main(String[] args) {
+	Userregistration usr=new Userregistration();
+	try
+	{
+		usr.registerUser("sai","america");
+	}
+	catch(InvalidCountryException e)
+	{
+		System.out.println(e);
+	}
+}
+}
